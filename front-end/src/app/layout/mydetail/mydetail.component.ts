82e1a60eb452b752e4ea5abac287c366c1e76541
my13312076223 <2213216619@qq.com>
2018-12-14 13:13:03 +0800

    p

--- front-end/src/app/layout/mydetail/mydetail.component.ts
+++ front-end/src/app/layout/mydetail/mydetail.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LQueries_ } from '@angular/core/src/render3/query';
 
 @Component({
   selector: 'app-mydetail',
   templateUrl: './mydetail.component.html',
   styleUrls: ['./mydetail.component.css']
 })
-export class MydetailComponent implements OnInit {
 
+export class MydetailComponent implements OnInit {
+  
   constructor() { }
 
   ngOnInit() {
