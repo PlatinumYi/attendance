b145388e57e7fc67edab3b7a9548968626935953
zhangkai <tjuzhangkai@tju.edu.cn>
2018-12-15 20:30:00 +0800

    完成服务层

--- front-end/src/app/service/apply.service.ts
+++ front-end/src/app/service/apply.service.ts
@@ -1,8 +1,8 @@
+import { ResponseData } from './../common/response-data';
 import { Absence } from './../domain/absence';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { ApiService } from './api.service';
-import { ResponseData } from '../common/response-data';
 
 @Injectable({
   providedIn: 'root'
@@ -29,7 +29,10 @@ export class ApplyService {
   //     .catch(this.handleError)
   // }
 
-  // 提交请假申请
+  /**
+   * 提交请假申请
+   * @param abs 
+   */
   addApply(abs: string): Promise<ResponseData> {
     console.log('fun '+ abs)
     const url = `${this.api_url}/apply`
@@ -42,6 +45,68 @@ export class ApplyService {
       //   var test = res.json()
       //   console.log( 'res '+test['error_code'])
       // })
+      .catch(this.handleError)
+  }
+
+  /**
+   * 这里的是已经通过，且请假时间包含当前时间的申请列表
+   */
+  getCurrentApply(): Promise<ResponseData> {
+    const url = `${this.api_url}/apply/current`
+    return this.http
+      .get(url, {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as ResponseData)
+      .catch(this.handleError)
+  }
+
+  /**
+   * 获取自己可以审核的假期
+   */
+  getPowerApply(): Promise<ResponseData> {
+    const url = `${this.api_url}/apply/power`
+    return this.http
+      .get(url, {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as ResponseData)
+      .catch(this.handleError)
+  }
+
+  /**
+   * 获取自己曾提交过的所有请假单
+   */
+  getSelfApply(): Promise<ResponseData> {
+    const url = `${this.api_url}/apply/self`
+    return this.http
+      .get(url, {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as ResponseData)
+      .catch(this.handleError)
+  }
+
+  /**
+   * 同意某申请
+   * @param id 
+   */
+  agreeApply(id: number): Promise<ResponseData> {
+    const url = `${this.api_url}/agree/${id}`
+    return this.http
+      .post(url, {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as ResponseData)
+      .catch(this.handleError)
+  }
+
+  /**
+   * 拒绝某申请
+   * @param id 
+   */
+  banApply(id: number):Promise<ResponseData>{
+    const url = `${this.api_url}/ban/${id}`
+    return this.http
+      .post(url, {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as ResponseData)
       .catch(this.handleError)
   }
 
